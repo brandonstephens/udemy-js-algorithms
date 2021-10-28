a6967c69eafb08e14ebee6c8bcd7ccf6848578c8
Brandon Stephens <brandonstephens@me.com>
2021-10-28 09:16:33 -0400

    wip

--- src/flatten.js
+++ src/flatten.js
@@ -1,19 +1,33 @@
 import { isTrue } from "./utils/tests.js";
 
-function flatten(arr, newArr = []){
-  console.log(newArr)
-  let foo = newArr
-  if (arr.length === 0) return newArr
-  if (typeof arr[0] !== 'object') {
-    foo = [arr[0], ...newArr]
+function flatten(arr, flat = [], leftOvers = []){
+  // console.log('arr', arr)
+  // console.log('flat', flat)
+  console.log('leftOvers', leftOvers)
+
+  // finished
+  if (arr.length === 0 && leftOvers.length === 0) return flat
+
+  // still have left overs
+  if (arr.length === 0 && leftOvers.length > 0) return flatten(leftOvers, flat)
+
+  // still have values
+  if (typeof arr[0] === 'object') {
+    return flatten(arr[0], flat, arr.slice(1))
+  } else {
+    return flatten(arr.slice(1), [...flat, arr[0]], leftOvers)
   }
-  return flatten(arr.slice(1), foo)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 // Tests
 ///////////////////////////////////////////////////////////////////////////////
-isTrue(flatten([1, 2, 3, [4, 5] ]).join() === [1, 2, 3, 4, 5].join())
-isTrue(flatten([1, [2, [3, 4], [[5]]]]).join() === [1, 2, 3, 4, 5].join())
-isTrue(flatten([[1],[2],[3]]).join() === [1,2,3]).join()
-isTrue(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]).join() === [1,2,3].join())
+// isTrue(flatten([1, 2, 3, [4, 5] ]).join() === [1, 2, 3, 4, 5].join())
+// isTrue(flatten([1, [2, [3, 4], [[5]]]]).join() === [1, 2, 3, 4, 5].join())
+// isTrue(flatten([[1],[2],[3]]).join() === [1,2,3]).join()
+// isTrue(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]).join() === [1,2,3].join())
+
+// console.log(flatten([1, 2, 3, [4, 5] ]))
+// console.log(flatten([1, [2, [3, 4], [[5]]]]))
+// console.log(flatten([[1],[2],[3]]))
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]))
